8d044ed3af487a2d7427a9202ea97cede34d3d35
asmarakhtar <75607633+asmarakhtar@users.noreply.github.com>
2021-01-28 10:06:40 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
+// Student Grades Assignment Start Code
 
 // HTML Variables
 let containerEl = document.getElementById("container");
 let outputEl = document.getElementById("output");
-let EndBtnEl = document.getElementById("endday");
-let plantfoodBtnEl = document.getElementById("plantfood");
-let pesticideBtnEl = document.getElementById("pesticide");
+let goBtnEl = document.getElementById("go");
 let menuEl = document.getElementById("menu");
-let ResetBtnEl= document.getElementById("reset");
-let infoBtnEl= document.getElementById("info");
-let checkBtnEl= document.getElementById("check");
+
 // Global Variable
-let sprouts = [10]
-let maxSprouts = 100; // sprout values should be b/t 0 and max
+let grades = [60, 70, 45, 20, 40, 90, 100, 30, 45, 75, 40, 80];
+let maxGrade = 100; // grade values should be b/t 0 and max
 function GradeRandom(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 }
@@ -23,33 +20,41 @@ drawArray();
 function drawArray() {
   let outputStr = "";
   let divHeight;
-  for (let i = 0; i < sprouts.length; i++) {
-    divHeight = (sprouts[i] / maxSprouts) * 600; // Scale grades to fit in array visualizer container
+  for (let i = 0; i < grades.length; i++) {
+    divHeight = (grades[i] / maxGrade) * 600; // Scale grades to fit in array visualizer container
     outputStr += `<div style="height:${divHeight}px"></div>`;
   }
   containerEl.innerHTML = outputStr;
 }
 
-// Button functions
-EndBtnEl.addEventListener("click", Endday);
-plantfoodBtnEl.addEventListener("click", plantfood);
-pesticideBtnEl.addEventListener("click", pesticide);
-ResetBtnEl.addEventListener("click", reset);
-infoBtnEl.addEventListener("click", mainMenu);
-checkBtnEl.addEventListener("click", check);
+// Main Menu & Go Button
+goBtnEl.addEventListener("click", mainMenu);
+
 function mainMenu() {
   // Get value of menu select element
   let selection = menuEl.value;
 
   // Take action based on menu selection
-  if (selection === "enddayoption") {
-    outputEl.innerHTML = "This button stimulates a growth cycle overnight, although plants need food to sustain growth!";
-  } else if (selection === "plantfoodoption") {
-    outputEl.innerHTML = "Plant food is great at helping your plants grow although too much plant food can bring insects";
-  } else if (selection === "pesticideoption") {
-    outputEl.innerHTML = "Pesticides are good on strong plants but they will have negative sideffects on weaker plants";
-  } else if (selection === "resetoption") {
-    outputEl.innerHTML = "Plants a new seed";
+  if (selection === "first40") {
+    firstTo40();
+  } else if (selection === "last50") {
+    lastTo50();
+  } else if (selection === "random100") {
+    randomTo100();
+  } else if (selection === "addRandom") {
+    addRandomGrade();
+  } else if (selection === "removeLast") {
+    removeLastGrade();
+  } else if (selection === "count50") {
+    countBelow50();
+  } else if (selection === "change50") {
+    lowGradesTo50();
+  } else if (selection === "increase10") {
+    increaseGradesBy10();
+  } else if (selection === "decrease10") {
+    decreaseGradesBy10();
+  } else if (selection === "remove50") {
+    removeGradesBelow50();
   }
 
   // Redraw array to show any changes
@@ -59,89 +64,85 @@ function mainMenu() {
 // ******************************************************
 // MENU SELECTION FUNCTIONS
 // ******************************************************
-function  Endday() {
-  //End day and stimulate small growth
-  for (let i = 0; i < sprouts.length; i++) {
-     
-    if (sprouts [i] == 0) {
-      sprouts[0]= 0;
-      outputEl.innerHTML = "There are no plants to grow";
-     }
-    else if (sprouts [i] <= 40) {
-    sprouts[0] += 3;
-    outputEl.innerHTML = "Your sprout grew overnight!"
-         
-      } else if (sprouts [i] >= 40) {
-        sprouts[0] -=1
-        outputEl.innerHTML = "Your sprouts need food!";
-    }
-  }
-  drawArray();
-}
-         
-    
-   function plantfood () {
-    for (let i = 0; i < sprouts.length; i++) {
-      //Feed plant food and stimulate large growth at a cost
-     if (sprouts [i] == 0) {
-      sprouts[0]= 0;
-      outputEl.innerHTML = "There are no plants to feed";
-     }
-      else if (sprouts [i] <= 60) {
-        sprouts[0]+=10;
-      outputEl.innerHTML = "The plant food had a great effect";
-      }
-    else if (sprouts [i] >= 60) {
-      sprouts[0] =0;
-      outputEl.innerHTML = "The plant food attracted bugs!";
-   }
-   drawArray();
-  }
+function firstTo40() {
+  // Set the grade of the first student to 40.
+  outputEl.innerHTML = "First grade to 40"; 
+  grades[0]=40;
 }
 
-
-
-  function pesticide () {
-    //Stimulate situational growth.
-    for (let i = 0; i < sprouts.length; i++) {
-     if (sprouts[i] == 0) {
-      outputEl.innerHTML = "There are no plants to spray";
-    }
-      else if (sprouts [i] < 60) {
-        sprouts[0] =0;
-      outputEl.innerHTML = "The pesticides killed the plants!";
-  } else if (sprouts [i] > 60) {
-    sprouts [0] += 3;
-    outputEl.innerHTML = "The pesticides killed the insects!"
-
-}
+function lastTo50() {
+  // Set the grade of the last student to 50.
+  outputEl.innerHTML = "Last grade to 50";
+  for (let i = 0; i < grades.length; i++)
+  grades[grades.length-1]=50;
   
-  drawArray();
-}
-  } 
-
-function reset () {
-  //Restart simulator
-  for (let i = 0; i < sprouts.length; i++) {
-    sprouts [0] = 1
-  outputEl.innerHTML = "A new seed has been planted!"
-}
-drawArray();
 }
 
-function check () {
-  for (let i = 0; i < sprouts.length; i++) {
-    if (sprouts[i] == 100) {
-     outputEl.innerHTML = "You did it!";
-   } else if (sprouts [i] > 100) {
-    outputEl.innerHTML = "You overshot!";
-   } else if (sprouts [i] < 100) {
-    outputEl.innerHTML = "You still can grow taller!";
-   }
+function randomTo100() {
+  // Set the grade of a random student to 100.
+  outputEl.innerHTML = "Random grade to 100";
+  grades[GradeRandom(0, grades.length)]=100;
+  
+}
 
+function addRandomGrade() {
+  // Add a random grade between 0 and 100 to the end of the array.
+  outputEl.innerHTML = "Add random grade";
+  grades.push(Math.floor((Math.random() * 100) + 1));
+}
+
+function removeLastGrade() {
+  // Remove the last grade.
+  outputEl.innerHTML = "Remove the last grade";
+   
+    grades.pop()
+
+}
+
+function countBelow50() {
+  // Count how many grades are below 50.  Output the result
+  let count = 0;
+  for (let i = 0; i < grades.length; i++) {
+   if (grades [i] < 50) {
+     count = count + 1;
+    } 
+  
+    
+  }
+  outputEl.innerHTML = "There are" +" " + count +" "+ "grades below 50"; 
+}
+
+function lowGradesTo50() {
+  // Change all grades that are below 50 to be equal to 50.
+  outputEl.innerHTML = "Change low grades to 50";
+  for (let i = 0; i < grades.length; i++) {
+    if(grades[i] < 50)
+    grades[i]= 50;
+}
+}
+
+function increaseGradesBy10() {
+  // Increase each grade by 10%.
+  outputEl.innerHTML = "Increase all grades by 10%";
+  for (let i = 0; i < grades.length; i++) {
+    grades[i]+=10;
+}
+}
+function decreaseGradesBy10() {
+  // Decrease each grade by 10%.
+  outputEl.innerHTML = "Decrease all grades by 10%";
+  for (let i = 0; i < grades.length; i++) {
+    grades[i]-=10;
 }
 }
 
 
-
-
+function removeGradesBelow50() {
+  // Remove all grades that are below 50.
+  outputEl.innerHTML = "Remove grades below 50";
+  for (let i = 0; i < grades.length; i++) {
+    if (grades [i] <50) {
+      grades.splice(i, 1)
+    }     
+  }
+}
